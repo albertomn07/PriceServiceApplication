b2f649cbcd82d6ee6f369bc9053450b71e404e78
albertomn07 <albertomn07@gmail.com>
2024-02-19 12:42:24 +0100

    Removed unused imports

--- src/main/java/com/example/inditex/application/port/out/PriceRepository.java
+++ src/main/java/com/example/inditex/application/port/out/PriceRepository.java
@@ -1,9 +1,6 @@
 package com.example.inditex.application.port.out;
 
 import com.example.inditex.domain.model.Price;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
